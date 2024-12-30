78b686aa5043fa7ee13f729d7d77d687ea26aa46
AINadolinsky <andrey.nadolinsky@gmail.com>
2024-12-30 13:19:35 +0300

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,6 @@
+#3rd program
+x = 2 * 2 + 2
+y = int('2') * (2 + 2)
+print(x)
+print(y)
+print(x == y)   
---
+++ task4.py
@@ -0,0 +1,5 @@
+#4th program
+x = '123.456'
+print(x[4:5])
+
+print(int(float(x) * 10) % 10)
